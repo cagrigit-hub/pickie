011935df5736e7a045036eece2fc9ca5e02e2a8f
cagrigit-hub <77447064+cagrigit-hub@users.noreply.github.com>
2021-12-17 22:01:22 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,9 @@
 import "../style/header.scss"
 import { getAuth, signOut } from "firebase/auth";
 import {initializeApp} from "firebase/app"
-
+import {auth} from "../firebase.js";
 function Headers({authed}) {
     function handleClick(){
-        const firebaseConfig = {
-            apiKey: "AIzaSyAT1QOBWf_UpBvJ2gi9-UeE5QYdBaesEs0",
-            authDomain: "moviedata-eacb8.firebaseapp.com",
-            databaseURL: "https://moviedata-eacb8-default-rtdb.europe-west1.firebasedatabase.app",
-            projectId: "moviedata-eacb8",
-            storageBucket: "moviedata-eacb8.appspot.com",
-            messagingSenderId: "948938628834",
-            appId: "1:948938628834:web:1f4c4f2d075e0547ccc8bb"
-          };
-          const app = initializeApp(firebaseConfig);
-        const auth = getAuth(app);
         signOut(auth).then(() => {
         // Sign-out successful.
         }).catch((error) => {
